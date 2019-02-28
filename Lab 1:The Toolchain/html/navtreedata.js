e4a07c7f8d55087d8ba5aa21c2823a7682fb8b96
NGrogs <ngrogan15@gmail.com>
2019-02-28 18:17:13 +0000

    added docs

---
+++ Lab 1:The Toolchain/html/navtreedata.js
@@ -0,0 +1,12 @@
+var NAVTREE =
+[
+  [ "Lab 1", "index.html", ]
+];
+
+var NAVTREEINDEX =
+[
+"index.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
